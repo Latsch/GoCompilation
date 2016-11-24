888d7300589ecac00165c78803671609151702b1
Sebastian Brossmer <latsch_general@web.de>
2016-11-24 12:59:15 +0100

    Added console input

--- XMLFIleCreator/main.go
+++ XMLFIleCreator/main.go
@@ -8,29 +8,29 @@ import (
 )
 
 
-func openXML(){
+func openXML(name string){
 	
-	xmlFile, err := os.Open("data.xml")
+	xmlFile, err := os.Open(name + ".xml")
 	if err != nil {
 		fmt.Printf("Error opening file:", err )
-		createXML()
+		createXML(name)
 		
 	}
 	defer xmlFile.Close()
 
 }
 
-func createXML(){
+func createXML(name string){
 	var n string
 	
 	fmt.Printf("\nDo you wish to create it? y/n : ")
 		
-		_, err := fmt.Scanf("%s", &n)
+		_, err := fmt.Scan(&n)
 		if err != nil {
 			panic(err)
 		}
 		if n == "y" {
-			_, err := os.Create("data.xml")
+			_, err := os.Create(name + ".xml")
 			if err != nil{
 				fmt.Printf("Error creating file:", err)
 			}
@@ -42,5 +42,12 @@ func createXML(){
 
 
 func main(){
-	openXML()
+	var name string
+	
+	fmt.Printf("Which file do you want to open?")
+	_, err := fmt.Scan(&name)
+	if err != nil{
+		fmt.Printf("Error on input:" , err)
+	}
+	openXML(name)
 }
